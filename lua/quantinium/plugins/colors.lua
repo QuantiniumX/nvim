a2c06c8f056bdf8c553d7da6c29dfe1652451364
QuantiniumX <vaibhavvx331@gmail.com>
2024-01-03 12:26:17 +0530

    added transparent background

--- lua/quantinium/plugins/colors.lua
+++ lua/quantinium/plugins/colors.lua
@@ -1,11 +1,33 @@
 return {
     {
-        "rose-pine/neovim",
-        lazy = false,
-  	priority = 1000,
-  	opts = {},
-    	config = function()
-      	vim.cmd([[colorscheme rose-pine]])
-        end,
+
+        {
+            "folke/tokyonight.nvim",
+            opts = {
+                style = "storm",
+                transparent = true,
+                styles = {
+                    sidebars = "transparent",
+                    floats = "transparent",
+                },
+            },
+            config = function(_, opts)
+                local tokyonight = require "tokyonight"
+                tokyonight.setup(opts)
+                tokyonight.load()
+            end,
+        },
+
+    },
+
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine',
+        config = function()
+            require('rose-pine').setup({
+                disable_background = true,
+            })
+            vim.cmd('colorscheme rose-pine')
+        end
     },
 }
